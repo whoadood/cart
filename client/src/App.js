315a68780d092a117395ae2440058790c22254fc
Castle <castlerock88@protonmail.com>
2022-04-29 22:42:18 -0400

    back to base

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,7 @@
-import { useCartContext } from "./context/CartContext";
-import { useFetch } from "./hooks/useFetch";
-import Product from "./components/Product";
-import { useEffect, useState } from "react";
-
 function App() {
-  const { cart, dispatch } = useCartContext();
-
-  const { data, isLoading, error } = useFetch(
-    "https://fakestoreapi.com/products",
-    []
-  );
-
   console.log(cart);
 
-  return (
-    <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="products-container">
-          {data.map((prod) => (
-            <Product key={prod.id} prod={prod} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import CartProvider from "./context/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
