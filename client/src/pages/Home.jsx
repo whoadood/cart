bd1c985a8c55e3ee1ba7f3acb23a8e2933438440
Castle <castlerock88@protonmail.com>
2022-05-02 01:03:51 -0400

    woking on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useProductContext } from "../context/ProductContext";
+import Product from "../components/Product";
 
 export default function Home() {
+  const { data } = useProductContext();
+  const bestSellers = data
+    .sort((a, b) => b.rating.count - a.rating.count)
+    .slice(0, 6);
+
+  console.log("home", bestSellers);
+
   return (
     <>
       <h2>Garage Sale</h2>
       <p>One persons trash is anothers treasure</p>
+      <ul>
+        {bestSellers?.map((prod) => (
+          <li key={prod.id}>
+            <Product prod={prod} />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
