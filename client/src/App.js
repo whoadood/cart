5201a805d9ce6432e269781c8df758672d37bb20
Castle <castlerock88@protonmail.com>
2022-05-02 22:11:52 -0400

    hashrouter for gh pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./pages/Products";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
